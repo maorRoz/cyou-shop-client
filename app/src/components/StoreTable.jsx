e196676b3d84095b580e6a0be768b6a582faa0a4
maorRoz <andorys@post.bgu.ac.il>
2018-07-31 11:45:48 +0300

    dadada

--- app/src/components/StoreTable.jsx
+++ app/src/components/StoreTable.jsx
@@ -4,7 +4,8 @@ import {Table} from 'semantic-ui-react'
 
 class StoreTable extends Component {
     getStoreList(data){
-        return data.map((store) => <Store key={store._id} id={store._id} name={store.name} address={store.address}
+        if(data.map === undefined) return null;
+         return data.map((store) => <Store key={store._id} id={store._id} name={store.name} address={store.address}
              openingHours={store.openingHours} managerName={store.managerName} refresh={this.props.refresh}/>)
     }
     render() {
